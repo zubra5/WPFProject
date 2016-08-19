a61227a64f7bf73931572f0aa07a08bff82e00a1
zubra5 <larissapflyug@gmail.com>
2016-08-19 07:39:45 +0600

    first

---
+++ WpfNewProject/Controls/NavButton.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfNewProject.Controls
+{
+    [ContentProperty("Items")]
+    /// <summary>
+    /// Interaction logic for NavButton.xaml
+    /// </summary>
+    public partial class NavButton : UserControl
+    {
+        public static readonly DependencyProperty TitleProperty =
+          DependencyProperty.Register("Title", typeof(string),
+          typeof(NavButton), new PropertyMetadata(default(string)));
+
+
+        public static readonly DependencyProperty ItemsSourceProperty =
+        ItemsControl.ItemsSourceProperty.AddOwner(typeof(NavButton));
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }      
+        public ItemCollection Items {
+            get { return lstBox.Items; }
+           
+        }
+        public NavButton()
+        {
+            InitializeComponent();
+        }
+
+        private string _title;
+        public string Title {
+            get { return _title; }
+            set {
+                _title = value;
+                txtBlock.Text = _title;
+            }
+        }
+              
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var url = new Uri("pack://application:,,,/Content/images/open.png");
+            image.Source = new BitmapImage(url);
+            lstBox.Height = Convert.ToInt32(Properties.Settings.Default.HeightMiddlePart);
+            
+        }
+    }
+}
---
+++ WpfNewProject/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfNewProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string ImageSource1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            StackPanel1.DataContext = new ExpanderListViewModel();
+        }
+
+        private void ListBoxItem_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("123 123 ");
+        }
+    }
+
+    public class ExpanderToBooleanConverter : IValueConverter
+    {
+        
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value == parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    
+    }
+    public class ExpanderListViewModel
+    {
+        public Object SelectedExpander { get; set; }
+    }
+}
